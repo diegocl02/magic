b2bc13b0993ddd238121021788ec29066db7dabd
Diego Cisneros <diegocl02@gmail.com>
2017-10-17 00:09:03 -0700

    a

--- src/game/gamesetup.js
+++ src/game/gamesetup.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import Gif from '../gif/react-gif';
 import '../App.css';
 
 import dice from '../img/alivedice.gif'
@@ -8,6 +7,12 @@ export class GameSetup extends Component{
   render(){
     return(
       <div className='setupholder'>
+        <div className='title'>
+          Magic Board
+        </div>
+        <div className='subtitle'>
+          by MiniCrayon
+        </div>
         <div className='randomgifholder'>
           <img className='randomgif' src={dice}></img>
         </div>
